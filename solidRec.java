16e32c915f5deb9a01e9b17a625b4c3d4bed1736
Abid Pathan <pabid4515@gmail.com>
2025-07-20 20:24:09 +0530

    Patterns

---
+++ solidRec.java
@@ -0,0 +1,11 @@
+public class solidRec{
+    public static void main(String[] args) {
+        System.out.println("Solid Rectangle Pattern ");
+        for(int i = 1;i<=4;i++){
+            for(int j = 1;j<=5;j++){
+                System.out.print(" * ");
+            }
+            System.out.println(" ");
+        }
+        
+    }}
